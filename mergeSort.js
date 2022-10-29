72583e8ecd2b80cc525e9206a2b6e433f9e7cfe5
mbright90 <michael.bright90@icloud.com>
2022-10-29 13:32:45 +0100

    Write initial mergeSort function

--- mergeSort.js
+++ mergeSort.js
@@ -11,8 +11,23 @@
 // if length < 2, return (array is only a single element)
 //
 // else RECURSIVE CASE
-// array 1 = mergesort(array.splice(first half))
-// array 2 = mergesort(array.splice(second half))
+// array 1 = mergeSort(array.splice(first half))
+// array 2 = mergeSort(array.splice(second half))
 // return merge of array 1 and array 2
 //
 // ######################################################
+
+function mergeSort(array) {
+  console.log(`array: ${array}`)
+  if (array.length <= 1) return array
+
+  const leftHalf = mergeSort(array.slice(0, array.length / 2))
+  const rightHalf = mergeSort(array.slice(array.length / 2))
+
+  const merged = leftHalf.concat(rightHalf)
+  merged.sort((first, second) => first - second)
+  console.log(`merged: ${merged}`)
+  return merged
+}
+
+console.log(mergeSort([3, 5, 8, 7]))
