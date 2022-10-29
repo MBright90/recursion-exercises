ffa6cf4e40873b3b1e087f6a4b8032766be59e24
mbright90 <michael.bright90@icloud.com>
2022-10-29 14:26:44 +0100

    Fix bug on maximum call stack exceeded

--- mergeSort.js
+++ mergeSort.js
@@ -1,11 +1,12 @@
 // #####################################################
+//
 // Initial Pseudo Code
 //
 // Enter an array object
 // If the array is a single item long, return that array
 // If the array is longer than a single item:
-//   Sort left half of array by recursively calling function on left half
-//   Sort right half of array by recursively call function on right half
+//   Sort left half of array by recursively calling function on left half until one item is left
+//   Sort right half of array by recursively call function on right half until one item is left
 // Merge two arrays together
 // Return merged array
 //
@@ -21,25 +22,33 @@
 //
 // ######################################################
 
-
 // Initial creation of mergeSort using the built in sort functions. Creating this first allowed
 // me to test the base and recursive cases were doing their expected behaviors.
 
-// function mergeSort(array) {
-//   if (array.length <= 1) return array
+function mergeSortBuiltIn(array) {
+  if (array.length <= 1) return array
 
-//   const leftHalf = mergeSort(array.slice(0, array.length / 2))
-//   const rightHalf = mergeSort(array.slice(array.length / 2))
+  const leftHalf = mergeSort(array.slice(0, array.length / 2))
+  const rightHalf = mergeSort(array.slice(array.length / 2))
 
-//   const merged = leftHalf.concat(rightHalf)
-//   merged.sort((first, second) => first - second)
-//   return merged
-// }
+  const mergedArray = leftHalf.concat(rightHalf)
+  mergedArray.sort((first, second) => first - second)
+  return mergedArray
+}
 
-function mergeSortRevised(array) {
+console.log(mergeSortBuiltIn([45, 12, 29, 74, 4, 5]))
 
+//  ######################################################
+//
+// Creation of mergeSort without using built in sort functions. Avoided use of unshift as this is performance heavy.
+
+
+function mergeSort(array) {
+
+    // base case
     if (array.length <= 1) return array
   
+    // recursive case
     const leftHalf = mergeSortRevised(array.slice(0, array.length / 2))
     const rightHalf = mergeSortRevised(array.slice(array.length / 2))
   
@@ -49,34 +58,30 @@ function mergeSortRevised(array) {
 
     // Continue merging until all items have been merged. When all items have been merged on one half,
     // add the entire opposite half to the array and break out of the loop
-    (() => {
+    return (() => {
         while (leftHalf.length >= leftIndex || rightHalf.length >= rightIndex) {
             console.log(mergedArray)
 
-            // If all the left half items have been included, add the entire right half of the array
-            if (leftHalf.length < leftIndex ) {
-                mergedArray.concat(rightHalf)
-                rightIndex = rightHalf.length + 1
-                leftIndex = leftHalf.length + 1
-                return
-            // If all the right half items have been merged, add the entire left half of the array
-            } else if (rightHalf.length === 0) {
-                mergedArray.concat(leftHalf)
-                rightIndex = rightHalf.length + 1
-                leftIndex = leftHalf.length + 1
-                return
+            // If all the left half items have been included, add the entire right half of the array and return the array
+            // to the next step in the call stack
+            if (leftHalf.length <= leftIndex ) {
+                mergedArray.push(...rightHalf.slice(rightIndex))
+                return mergedArray
+            // If all the right half items have been merged, add the entire left half of the array and return the array
+            // to the next step in the call stack
+            } else if (rightHalf.length <= rightIndex) {
+                mergedArray.push(...leftHalf.slice(leftIndex))
+                return mergedArray
             // If the next item in the left half is less than the next item in the right half, merge the left item
             } else if (leftHalf[leftIndex] < rightHalf[rightIndex]) {
-                mergedArray.push(leftHalf[leftIndex])
+                mergedArray[mergedArray.length] = leftHalf[leftIndex]
                 leftIndex += 1
             // If the next item in the right half is less than the next item in the left half, merge the right item
             } else {
-                mergedArray.push(rightHalf[rightIndex])
+                mergedArray[mergedArray.length] = rightHalf[rightIndex]
                 rightIndex += 1
             }
     }})()
-
-    return mergedArray
 }
 
-console.log(mergeSortRevised([1, 3]))
+console.log(mergeSortRevised([33, 21, 43, 15, 98, 76, 45, 32]))
